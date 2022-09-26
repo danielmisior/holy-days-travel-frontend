ce2807fe6f1f623849a6f7ba3c7db7eda462b088
Daniel Misior <daniel.misior09@gmail.com>
2022-09-26 10:40:36 +0200

    Initial commit

---
+++ src/main/java/com/frontend/holydaystravel/HolyDaysTravelFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.frontend.holydaystravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolyDaysTravelFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolyDaysTravelFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frontend/holydaystravel/HolyDaysTravelFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frontend.holydaystravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolyDaysTravelFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
